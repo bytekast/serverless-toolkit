b8f01ed50ed90a6336506b54f0546b8cfce94435
Rowell Belen <rowell.belen@serverless.com>
2019-12-07 17:14:00 -0700

    Minor updates

--- templates/groovy-simple/src/main/groovy/com/serverless/Handler.groovy
+++ templates/groovy-simple/src/main/groovy/com/serverless/Handler.groovy
@@ -2,25 +2,26 @@ package com.serverless
 
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2ProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2ProxyResponseEvent
+import groovy.json.JsonOutput
 import groovy.transform.CompileStatic
 import org.apache.log4j.Logger
 
 @CompileStatic
-class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+class Handler implements RequestHandler<APIGatewayV2ProxyRequestEvent, APIGatewayV2ProxyResponseEvent> {
 
   private static final Logger LOG = Logger.getLogger(Handler.class)
 
+  @CompileStatic
   @Override
-  ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-    LOG.info("received: " + input)
-    Response responseBody = Response.builder()
-        .message('Go Serverless v1.x! Your function executed successfully!')
-        .input(input)
-        .build()
-    return ApiGatewayResponse.builder()
-        .statusCode(200)
-        .body(responseBody.toJson())
-        .headers(['X-Powered-By': 'AWS Lambda & serverless'])
-        .build()
+  APIGatewayV2ProxyResponseEvent handleRequest(APIGatewayV2ProxyRequestEvent event, Context context) {
+    final response = new APIGatewayV2ProxyResponseEvent()
+    response.setStatusCode(200)
+    response.setBody(JsonOutput.toJson([
+      message: 'Go Serverless v1.x! Your function executed successfully!'
+    ]))
+    return response
   }
+
 }
