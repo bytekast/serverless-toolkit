ed0a1dff3a2f19281ef74b3af3edc91a57bc78a3
Rowell Belen <rowell.belen@serverless.com>
2020-01-11 21:46:18 -0700

    Add micronaut template

---
+++ templates/java-micronaut-monolith/src/main/java/serverless/PingController.java
@@ -0,0 +1,18 @@
+package serverless;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller("/ping")
+public class PingController {
+	
+	private Logger log = LoggerFactory.getLogger(PingController.class);
+
+    @Get("/")
+    public String ping() {
+    	log.trace("Received a ping.");
+    	return "{\"pong\":true}";
+    }
+}
