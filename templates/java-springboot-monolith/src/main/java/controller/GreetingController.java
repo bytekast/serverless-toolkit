e15f5396fc3d31cfca4c4bcb07416a00c3fba534
Rowell Belen <developer@bytekast.com>
2020-02-22 00:12:03 -0700

    springboot monolith sample

---
+++ templates/java-springboot-monolith/src/main/java/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@EnableWebMvc
+public class GreetingController {
+
+  private final AtomicLong counter = new AtomicLong();
+
+  @GetMapping("/greeting")
+  public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return new Greeting(counter.incrementAndGet(), String.format("Hello, %s!", name));
+  }
+}
